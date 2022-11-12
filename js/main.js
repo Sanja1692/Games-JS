5fbcedee928376615a8780845fce5bb09031deb3
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-12 14:53:14 +0100

    j

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 (function() {
-    let startNtn = $('.startBtn');
+    let startBtn = $('.startBtn');
     let mainInput = $('.mainInput');
     let allLines = $('.line');
+
+    startBtn.on('click', startGame);
+
+    function startGame() {
+        $(this).hide();
+    }
 })
