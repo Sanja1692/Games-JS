a12ca9569a438e7dfcc1efcf202b832470399a5d
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-13 22:15:59 +0100

    nasumicne reci

--- js/main.js
+++ js/main.js
@@ -15,8 +15,22 @@
         let level = 6; //1-20 
         //insert spans
         function insertSpans() {
-
+            for (let i = 0; i < allLines.length; i++) {
+                let rand = Math.floor(Math.random() * 20);
+                if (rand <= level) {
+                    let text = chooseText();
+                    $(allLines[i]).append(`<span>${text}</span>`);
+                }
+            }
         }
         insertSpans();
+
+        function chooseText() {
+            let rand = Math.floor(Math.random() * typingWords.length);
+            let savedText = typingWords[rand];
+            typingWords.splice(rand, 1);
+
+            return typingWords[rand];
+        }
     }
 })()
