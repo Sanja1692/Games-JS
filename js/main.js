37b4d9ad5726f4a600e9a5e2efcaa7fb201baadb
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-13 22:06:08 +0100

    d

--- js/main.js
+++ js/main.js
@@ -7,5 +7,16 @@
 
     function startGame() {
         $(this).hide();
+        //setup
+        let speed = 1;
+        let textLength = 3;
+        let typingWords = words.filter(word => word.length == textLength);
+        // console.log(typingWords);
+        let level = 6; //1-20 
+        //insert spans
+        function insertSpans() {
+
+        }
+        insertSpans();
     }
-})
+})()
